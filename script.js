179a2ec214b462aded48fbc52f00491b1d3edb5c
Benji <snowrepor@gmail.com>
2021-12-01 17:56:41 -0700

    js fixs from sandbox

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 $(document).ready(function() {
     console.log("ready");
-// query url weather app
-var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
-"45e45c0bb2ef540df33fa21a29aafa8a";
+// variables that are global
+var cityInput="";
+var queryUrl="";
+
+storeCities();
+
+// click after city is entered
+$("#cityInput").on("click", function(event) {
+    event.preventDefault();
+    console.log("you have clicked the button");
+
+    //
+    cityInput = $("input").val();
+    console.log(cityInput, "cityInput")
+});
+//
+
+// function that retrieves cities
+function storeCities () {
+
+// query url open weather app
+var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q="
++ cityInput + "&appid=" + "45e45c0bb2ef540df33fa21a29aafa8a"
+console.log(queryUrl);
+
 // ajax call
 $.ajax({
     url: queryUrl,
     method: "GET",
 }).then(function(response) {
     console.log(response);
-    for(var i = 0; i < response.data.length; i++) { // loop responses
-        console.log("TITLE' response.data[i]. title");
-    }
-});
-});
+    console.log(response.city)
+}) // end ajax
+} //end store city function
+
+// append details to the site 
+$("#dailyWeather").append(
+    //styling
+    "<div class='col s12 m6'>"
+
+    // weather contents 
+    +  "<h2>" + response.city + "</h2>"
+
+    //close out card styling 
+    + "</div>"
+    ); // end of append 
+
+}); // end of document ready function
